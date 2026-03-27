d8455e5891dc94ec6c4cb44ba621bef75f7b2a6d
alexandre <alexandre020602@gmail.com>
2026-03-27 12:37:41 -0300

    configuring routes and styles

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { icons } from '@/constants/icons'
+import TabIcon from '@/components/TabIcon'
+
+const _Layout = () => {
+    return (
+        <Tabs screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 0,
+                marginTop: 5,
+                paddingTop: 0,
+                paddingBottom: 0,
+            },
+            tabBarStyle: {
+                backgroundColor: "#0f0D23",
+                borderRadius: 50,
+                marginHorizontal: 10,
+                marginBottom: 36,
+                height: 56,
+                position: "absolute",
+                overflow: "hidden",
+                paddingVertical: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+            }
+
+        }}>
+            <Tabs.Screen name="index" options={{
+                title: 'Home',
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (<TabIcon
+                    focused={focused}
+                    icon={icons.home}
+                    title='Home'
+                />)
+
+            }} />
+            <Tabs.Screen name="search" options={{
+                title: 'Search',
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (<TabIcon
+                    focused={focused}
+                    icon={icons.search}
+                    title='Search'
+                />)
+            }} />
+            <Tabs.Screen name="saved" options={{
+                title: 'Saved',
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (<TabIcon
+                    focused={focused}
+                    icon={icons.save}
+                    title='Saved'
+                />)
+            }} />
+            <Tabs.Screen name="profile" options={{
+                title: 'Profile',
+                headerShown: false,
+                tabBarIcon: ({ focused }) => (<TabIcon
+                    focused={focused}
+                    icon={icons.person}
+                    title='Profile'
+                />)
+            }} />
+        </Tabs>
+    )
+}
+
+export default _Layout
