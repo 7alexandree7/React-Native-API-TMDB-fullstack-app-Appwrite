c0aa761a808b7147fa9275a7eba5f263aa37b4f8
alexandre <alexandre020602@gmail.com>
2026-03-28 12:59:35 -0300

    feat: add useFetch custom hook with auto-fetch and reset support

---
+++ services/api.ts
@@ -0,0 +1,36 @@
+import { envConfig } from "@/config/env";
+import { TMDBConfig } from "@/interfaces/interfaces";
+
+
+
+export const TMDB_CONFIG: TMDBConfig = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: envConfig.MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${envConfig.MOVIE_API_KEY}`
+    }
+}
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+
+    const endpoint = query ?
+        `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :
+        `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    try {
+        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/${endpoint}`, {
+            method: 'GET',
+            headers: TMDB_CONFIG.headers
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch movies')
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        
+    }
+}
