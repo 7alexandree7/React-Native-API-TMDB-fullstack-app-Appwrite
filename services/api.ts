bc03c8b0c40016cc0d53e48f8bfa8ca46ec7715a
alexandre <alexandre020602@gmail.com>
2026-03-28 13:01:33 -0300

    feat: add TMDB config and fetchMovies service

--- services/api.ts
+++ services/api.ts
@@ -31,6 +31,7 @@ export const fetchMovies = async ({ query }: { query: string }) => {
         const data = await response.json()
         return data
     } catch (error) {
-        
+        console.error('Error fetching movies:', error)
+        throw error
     }
 }
