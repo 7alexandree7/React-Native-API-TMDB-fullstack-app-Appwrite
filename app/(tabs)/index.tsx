a147edb3f3ec420d9e38c1317bd3f63c4bf23fe3
alexandre <alexandre020602@gmail.com>
2026-03-29 08:55:29 -0300

    feat: add home screen with movie

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,52 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import useFetch from "@/hooks/useFetch";
+import { fetchMovies } from "@/services/api";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 
 export default function Index() {
+
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: "" }))
 
   const router = useRouter();
 
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0" />
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-           onPress={() => router.push("/search")}
-           placeholder="Search for a movie..."
-            />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+        numColumns={3}
+        columnWrapperStyle={{ justifyContent: "flex-start", gap: 20, marginBottom: 10, paddingHorizontal: 5 }}
+        className="mt-2 pb-32 px-5"
+        ListHeaderComponent={
+          <>
+            <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+            {moviesLoading ? (
+              <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+            ) : moviesError ? (
+              <Text className="text-white text-center mt-10">Error {moviesError}</Text>
+            ) : (
+              <>
+                <SearchBar
+                  onPress={() => router.push("/search")}
+                  placeholder="Search for a movie..."
+                />
+                <Text className="text-white text-lg font-semibold mt-5 mb-3">Latest Movies</Text>
+              </>
+            )}
+          </>
+        }
+      />
     </View>
   );
 }
