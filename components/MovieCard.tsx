896b0b33016df0c2922aa2334fd8a590fb535fd1
alexandre <alexandre020602@gmail.com>
2026-03-29 08:54:56 -0300

    feat: add MovieCard component

---
+++ components/MovieCard.tsx
@@ -0,0 +1,34 @@
+import { icons } from '@/constants/icons'
+import { Movie } from '@/interfaces/interfaces'
+import { Link } from 'expo-router'
+import React from 'react'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+
+const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
+    return (
+        <Link href={{ pathname: '/movie/[id]', params: { id } }} asChild>
+            <TouchableOpacity className="w-[30%]">
+                <Image
+                    className='w-full h-52 rounded-lg' resizeMode='cover'
+                    source={{
+                        uri: poster_path
+                            ?
+                            `https://image.tmdb.org/t/p/w500${poster_path}`
+                            :
+                            "https://placehold.co/600x400/1a1a1a/ffffff.png"
+                    }}
+                />
+                <Text numberOfLines={1} className="text-white text-sm font-semibold mt-2">{title}</Text>
+                <View className='flex-row items-center justify-start gap-x-1'>
+                    <Image source={icons.star} className='size-4' />
+                    <Text className='text-gray-400 text-xs'>{vote_average.toFixed(1)}</Text>
+                </View>
+                <View className='flex-row items-center justify-between'>
+                    <Text className='text-gray-400 text-xs'>{release_date.split('-')[0]}</Text>
+                </View>
+            </TouchableOpacity>
+        </Link>
+    )
+}
+
+export default MovieCard
